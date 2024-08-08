1256cbcac3313e9e7b2843827843a48244b15e5a
Nazmul Hossain <nazmulhossain914@gmail.com>
2024-08-08 19:04:55 +0600

    Uploading Files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function showText(){
+    document.getElementById('s_text').innerHTML = "I am not a text, I am a text. :)";
+}
+
+function dateShow(){
+    document.getElementById('date').innerHTML = Date();
+}
+
+function myName(){
+    document.getElementById('name').innerHTML = "I am Knowledgeless, That's all my name.";
+}
+
+function hideText(){
+    document.getElementById('hide').style.display = "none";
+}
